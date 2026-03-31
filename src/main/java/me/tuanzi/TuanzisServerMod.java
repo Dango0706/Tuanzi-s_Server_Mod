299460b6fc3575771f0f5beac47fbc028390707d
Tuanzi <1261698640@qq.com>
2026-03-31 08:59:19 +0800

    国际化.

--- src/main/java/me/tuanzi/TuanzisServerMod.java
+++ src/main/java/me/tuanzi/TuanzisServerMod.java
@@ -5,7 +5,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class TuanzisServerMod implements ModInitializer {
-    public static final String MOD_ID = "template-mod";
+    public static final String MOD_ID = "tuanzis-server-mod";
 
     // This logger is used to write text to the console and the log file.
     // It is considered best practice to use your mod id as the logger's name.
@@ -18,6 +18,6 @@ public class TuanzisServerMod implements ModInitializer {
         // However, some things (like resources) may still be uninitialized.
         // Proceed with mild caution.
 
-        LOGGER.info("Hello Fabric world!");
+//        LOGGER.info("Hello Fabric world!");
     }
 }
